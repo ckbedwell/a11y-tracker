1e6e7361e474ea578dcc4973362566b5e2bc2956
Chris Bedwell <christopher.bedwell@grafana.com>
2024-01-11 13:46:54 +0000

    Added scenes supporting app

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,8 +1,8 @@
 import React, { ChangeEvent } from 'react';
 import { InlineField, Input, Select } from '@grafana/ui';
 import { QueryEditorProps, SelectableValue } from '@grafana/data';
-import { DataSource } from '../datasource';
-import { MyDataSourceOptions, MyQuery } from '../types';
+import { DataSource } from 'datasource';
+import { MyDataSourceOptions, MyQuery } from 'types';
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
